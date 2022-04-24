a4ee0fdcc42ceda7003592e8bbd8da9ab709244f
357473 <81597261+357473@users.noreply.github.com>
2022-04-24 20:51:18 +0100

    Update Main.java

--- Part 9/part09-Part09_03.Warehousing/src/main/java/Main.java
+++ Part 9/part09-Part09_03.Warehousing/src/main/java/Main.java
@@ -6,8 +6,7 @@ public class Main {
         ProductWarehouseWithHistory juice = new ProductWarehouseWithHistory("Juice", 1000.0, 1000.0);
         juice.takeFromWarehouse(11.3);
         juice.addToWarehouse(1.0);
-        //System.out.println(juice.history()); // [1000.0, 988.7, 989.7]
-
+        
         juice.printAnalysis();
     }
 }
